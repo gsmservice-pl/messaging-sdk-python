1cf0b54d3a0574cf59d3faaede9ff62ed0b8622d
tommekk83 <tomasz@gsmservice.pl>
2024-10-24 21:34:01 +0200

    Update version to 2.0.4
    Bumped version number from 2.0.1 to 2.0.4 across various files. This update includes changes to _version.py, pyproject.toml, and sdkconfiguration.py. Updated user agent to reflect new SDK and openapi_doc versions.

--- src/gsmservice_gateway/sdkconfiguration.py
+++ src/gsmservice_gateway/sdkconfiguration.py
@@ -30,10 +30,10 @@ class SDKConfiguration:
     server_url: Optional[str] = ""
     server: Optional[str] = ""
     language: str = "python"
-    openapi_doc_version: str = "1.0.1"
-    sdk_version: str = "2.0.1"
+    openapi_doc_version: str = "1.0.2"
+    sdk_version: str = "2.0.4"
     gen_version: str = "2.438.15"
-    user_agent: str = "speakeasy-sdk/python 2.0.1 2.438.15 1.0.1 gsmservice-gateway"
+    user_agent: str = "speakeasy-sdk/python 2.0.4 2.438.15 1.0.2 gsmservice-gateway"
     retry_config: OptionalNullable[RetryConfig] = Field(default_factory=lambda: UNSET)
     timeout_ms: Optional[int] = None
 
